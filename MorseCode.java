d7bba08fde2da7dbab7f4a5fb724ce9e2460ba80
Andrew Bevelhymer <andrew.bevelhymer@okstate.edu>
2016-09-27 18:29:13 -0500

    fixed issue in encode method where two spaces wouldnt print, added for loops in decode method to print

--- MorseCode.java
+++ MorseCode.java
@@ -92,6 +92,11 @@ public class MorseCode
           System.out.print(morse[j] + " ");
           break;
         }
+        else if (letter.equals(" "))
+        {
+          System.out.print(" ");
+          break;
+        }
         else
         {
           continue;
@@ -130,120 +135,19 @@ public class MorseCode
         // Defining letter variable
         String letter = character[j];
 
-        // Using switch statemtent with letter as the condition
-        switch(letter)
+        // Loop through morse array
+        for (int k = 0; k < morse.length; k++)
         {
-          case ".-":
-            System.out.print("a");
+          // Check if k in morse equals the letter and print the english equivalent
+          if (letter.equals(morse[k]))
+          {
+            System.out.print(english[k]);
             break;
-          case "-...":
-            System.out.print("b");
-            break;
-          case "-.-.":
-            System.out.print("c");
-            break;
-          case "-..":
-            System.out.print("d");
-            break;
-          case ".":
-            System.out.print("e");
-            break;
-          case "..-.":
-            System.out.print("f");
-            break;
-          case "--.":
-            System.out.print("g");
-            break;
-          case "....":
-            System.out.print("h");
-            break;
-          case "..":
-            System.out.print("i");
-            break;
-          case ".---":
-            System.out.print("j");
-            break;
-          case "-.-":
-            System.out.print("k");
-            break;
-          case ".-..":
-            System.out.print("l");
-            break;
-          case "--":
-            System.out.print("m");
-            break;
-          case "-.":
-            System.out.print("n");
-            break;
-          case "---":
-            System.out.print("o");
-            break;
-          case ".--.":
-            System.out.print("p");
-            break;
-          case "--.-":
-            System.out.print("q");
-            break;
-          case ".-.":
-            System.out.print("r");
-            break;
-          case "...":
-            System.out.print("s");
-            break;
-          case "-":
-            System.out.print("t");
-            break;
-          case "..-":
-            System.out.print("u");
-            break;
-          case "...-":
-            System.out.print("v");
-            break;
-          case ".--":
-            System.out.print("w");
-            break;
-          case "-..-":
-            System.out.print("x");
-            break;
-          case "-.--":
-            System.out.print("y");
-            break;
-          case "--..":
-            System.out.print("z");
-            break;
-          case ".----":
-            System.out.print("1");
-            break;
-          case "..---":
-            System.out.print("2");
-            break;
-          case "...--":
-            System.out.print("3");
-            break;
-          case "....-":
-            System.out.print("4");
-            break;
-          case ".....":
-            System.out.print("5");
-            break;
-          case "-....":
-            System.out.print("6");
-            break;
-          case "--...":
-            System.out.print("7");
-            break;
-          case "---..":
-            System.out.print("8");
-            break;
-          case "----.":
-            System.out.print("9");
-            break;
-          case "-----":
-            System.out.print("0");
-            break;
-          case " ":
-            System.out.print(" ");
-            break;
+          }
+          else
+          {
+            continue;
+          }
         }
       }
     }
